3723859c4930c8c768f00269b53b8b474934bcb9
AshwaniSingh12 <as3141538@gmail.com>
2024-10-26 21:15:20 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
         </div>
         <About/>
         <Education/>
-        <Tech/>
+        {/* <Tech/> */}
         <Works/>
         <Feedbacks/> 
         <div className="relative z-0">
